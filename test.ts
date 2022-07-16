5f9f750425002b4fdbfb8b3649d4d8cf3cc75d7e
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2022-07-16 18:36:49 +0000

    test file

---
+++ test.ts
@@ -0,0 +1,16 @@
+import RestManager from "./packages/rest/RestManager";
+
+const api = new RestManager({
+  token: process.env.TOKEN,
+});
+
+for (let i = 1; i <= 100; i++) {
+  api.post("/channels/997202426820038756/messages", {
+    body: { content: "Hello World " + i },
+  });
+  api.post("/channels/997933194781409380/messages", {
+    body: { content: "Hello Second Channel " + i },
+  });
+}
+
+
